a763cee11bedc558e485401d7d7133ec65b1214f
Manuela Florez <manuela@creativosdigitales.co>
2024-10-19 17:32:34 -0500

    ajustes vistas

--- obj/Debug/net8.0/Bingoo.AssemblyInfo.cs
+++ obj/Debug/net8.0/Bingoo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bingoo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+395235d80a3805a18f33844d1d9c2a010e84c6c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a870d68c71a3a7b28a41707ddfa028620901b4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bingoo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bingoo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
